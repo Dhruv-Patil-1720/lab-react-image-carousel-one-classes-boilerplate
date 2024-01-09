b75698af401382327006658a66c9e8d1aa6f6123
Dhruv Unmesh Patil <universo_esplorar@Dhruvs-MacBook-Air.local>
2024-01-09 16:10:47 +0530

    done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,53 @@
 import React, { Component } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-// you can explore more - and check as how to use materiul ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// implement the class below
 class Carousel extends Component {
-  
+    constructor() {
+        super();
+        this.state = {
+            index: 0
+        };
+    }
+
+    handlePrev = () => {
+        this.setState({
+            index: (this.state.index - 1 + images.length) % images.length
+        });
+    };
+
+    handleNext = () => {
+        this.setState({
+            index: (this.state.index + 1) % images.length
+        });
+    };
+
+    render() {
+        let currentImage = images[this.state.index];
+
+        return (
+            <>
+                <div className="mainContainer">
+                    <div className="arrowIcon"><ArrowBackIosIcon onClick={this.handlePrev} /></div>
+                    <div className="imageBox">
+                        <img src={currentImage.img} alt="" />
+                        
+                    </div>
+                    <div className="imageContainer">
+                        <div className="titleBox">
+                            <p>{currentImage.title}</p>
+                        </div>
+                        <div className="subTitleBox">
+                            <p>{currentImage.subtitle}</p>
+                        </div>
+                    </div>
+                    <div className="arrowIcon1"><ArrowForwardIosIcon onClick={this.handleNext} /></div>
+                </div>
+            </>
+        );
+    }
 }
 
 export default Carousel;
